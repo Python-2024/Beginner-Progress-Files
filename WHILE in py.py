ff2fede9af28f84c18f44cb31475cddbe57d40c0
Himesh Gupta <155180662+Python-2024@users.noreply.github.com>
2025-08-15 00:31:43 +0530

    Add files via upload

---
+++ WHILE in py.py
@@ -0,0 +1,13 @@
+i = 10
+while i>0:
+    print("Reverse ",i)
+    i -= 1
+else:
+    print("Done!")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+while True:
+  number = int(input("Enter a positive number: "))
+  print(number)
+  if not number > 0:
+    break
