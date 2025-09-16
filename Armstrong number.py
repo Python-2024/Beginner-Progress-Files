6103ea9f9749c4f8704bea5fae98915c97e65228
Himesh Gupta <155180662+Python-2024@users.noreply.github.com>
2025-09-16 21:48:46 +0530

    Update Armstrong number.py

--- Armstrong number.py
+++ Armstrong number.py
@@ -1,9 +1,9 @@
 #Arm- strong number of digit 3
 #153 = 1^3 + 5^3 + 3^3 = 153
 #370 = 3^3 + 7^3 + 0^3 = 370
-#__________________________________
+#_______________________________________________________________________________________________________________________
 
-
+#old code
 number = str(input("Enter a number: ")).strip()
 power = int(input("Enter a power: "))
 r = 0
@@ -11,3 +11,18 @@ r = 0
 for i in number:
     r += int(i)**power
 print(r)
+
+
+
+#_______________________________________________________________________________________________________________________
+
+
+#here this is the updated code
+while True:
+    number = str(input("Enter a number: ")).strip()
+    power = len(number)
+
+    r = 0
+    for i in number:
+        r += int(i)**power
+    print(r)
